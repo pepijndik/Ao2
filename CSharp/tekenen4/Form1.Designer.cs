8fc8aa32e7542e73eafde7e52303436b23ec260b
pepijn <pepijn@pdik.nl>
2019-09-09 12:10:50 +0200

    C# Tekenen 4

---
+++ CSharp/tekenen4/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace tekenen4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtX = new System.Windows.Forms.TextBox();
+            this.txtY = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtYeind = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtXeind = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(31, 45);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(476, 286);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(646, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // txtX
+            // 
+            this.txtX.Location = new System.Drawing.Point(666, 66);
+            this.txtX.Name = "txtX";
+            this.txtX.Size = new System.Drawing.Size(100, 22);
+            this.txtX.TabIndex = 2;
+            // 
+            // txtY
+            // 
+            this.txtY.Location = new System.Drawing.Point(666, 103);
+            this.txtY.Name = "txtY";
+            this.txtY.Size = new System.Drawing.Size(100, 22);
+            this.txtY.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(646, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Y";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(604, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(184, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Geef coordinaten van begin";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(604, 159);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(177, 17);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Geef coordinaten van Eind";
+            // 
+            // txtYeind
+            // 
+            this.txtYeind.Location = new System.Drawing.Point(666, 217);
+            this.txtYeind.Name = "txtYeind";
+            this.txtYeind.Size = new System.Drawing.Size(100, 22);
+            this.txtYeind.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(646, 217);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(17, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Y";
+            // 
+            // txtXeind
+            // 
+            this.txtXeind.Location = new System.Drawing.Point(666, 180);
+            this.txtXeind.Name = "txtXeind";
+            this.txtXeind.Size = new System.Drawing.Size(100, 22);
+            this.txtXeind.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(646, 180);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(14, 17);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "x";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(666, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Teken de lijn";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtYeind);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtXeind);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtY);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtX);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtX;
+        private System.Windows.Forms.TextBox txtY;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtYeind;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtXeind;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CSharp/tekenen4/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekenen4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+            //begin
+            int x = int.Parse(txtX.Text);
+            int y = int.Parse(txtY.Text);
+            //Eind
+            int xeind = int.Parse(txtXeind.Text);
+            int yeind = int.Parse(txtYeind.Text);
+            Pen penred = new Pen(Color.Red);
+            Graphics g = panel1.CreateGraphics();
+            g.DrawLine(penred, x, y, xeind, yeind);
+        }
+    }
+}
