3fc7a73bf1ea749341ea6c4f000a2aa7ab8746d0
pepijn <pepijn@pdik.nl>
2019-09-09 11:17:03 +0200

    Csharp 2

---
+++ CSharp/tekenen1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tekenen1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = panel1.CreateGraphics();
+
+            //Draw Circel 
+            SolidBrush sb_rood = new SolidBrush(Color.Red);
+            g.FillEllipse(sb_rood, 100, 70,100,100);
+
+            //Draw rectangle
+
+            g.FillRectangle(sb_rood, 165, 130, 35,250);
+        }
+        
+    }
+}
