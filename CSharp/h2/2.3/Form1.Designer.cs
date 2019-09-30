61cba4f5c207b85b90fe636d6b821735c6d1d7e4
pepijn <pepijn@pdik.nl>
2019-09-30 11:45:07 +0200

    2.3
    Use String Array to show names.
    Foreach to print out every student

---
+++ CSharp/h2/2.3/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace _2._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnShow = new System.Windows.Forms.Button();
+            this.lstbox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(253, 38);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(179, 39);
+            this.btnShow.TabIndex = 0;
+            this.btnShow.Text = "Laat studenten zien";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.BtnShow_Click);
+            // 
+            // lstbox
+            // 
+            this.lstbox.FormattingEnabled = true;
+            this.lstbox.ItemHeight = 16;
+            this.lstbox.Location = new System.Drawing.Point(142, 106);
+            this.lstbox.Name = "lstbox";
+            this.lstbox.Size = new System.Drawing.Size(409, 260);
+            this.lstbox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstbox);
+            this.Controls.Add(this.btnShow);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.ListBox lstbox;
+    }
+}
+
---
+++ CSharp/h2/2.3/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2._3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()  
+        {
+            InitializeComponent();
+        }
+        String[] Studenten = new String[] { "Pepijn", "Samir", "bob", "Thijs", "Mohammed" };
+        private void BtnShow_Click(object sender, EventArgs e)
+        {
+            lstbox.Items.Clear();
+            for (int i = 0; i < Studenten.Length; i++)
+            {
+                lstbox.Items.Add(Studenten[i]);
+            }
+        }
+    }
+}
