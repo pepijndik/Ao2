6dcc326c6b9ca805fdd552eae9426b9a820e2af9
pepijn <pepijn@pdik.nl>
2019-09-30 11:26:25 +0200

    Csharp Opdracht 2.2
    Werkende versie met functies

--- CSharp/h1/2.1/Form1.Designer.cs
+++ CSharp/h1/2.1/Form1.Designer.cs
@@ -28,37 +28,40 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.lstBox = new System.Windows.Forms.ListBox();
             this.btn1 = new System.Windows.Forms.Button();
             this.btn2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // listBox1
+            // lstBox
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(127, 125);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(454, 212);
-            this.listBox1.TabIndex = 0;
+            this.lstBox.FormattingEnabled = true;
+            this.lstBox.ItemHeight = 16;
+            this.lstBox.Location = new System.Drawing.Point(164, 86);
+            this.lstBox.Name = "lstBox";
+            this.lstBox.Size = new System.Drawing.Size(454, 212);
+            this.lstBox.TabIndex = 0;
+            this.lstBox.SelectedIndexChanged += new System.EventHandler(this.LstBox_SelectedIndexChanged);
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(214, 52);
+            this.btn1.Location = new System.Drawing.Point(199, 52);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(114, 23);
+            this.btn1.Size = new System.Drawing.Size(123, 28);
             this.btn1.TabIndex = 1;
             this.btn1.Text = "laag naar hoog";
             this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.Btn1_Click);
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(428, 52);
+            this.btn2.Location = new System.Drawing.Point(412, 52);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(124, 23);
+            this.btn2.Size = new System.Drawing.Size(140, 28);
             this.btn2.TabIndex = 2;
             this.btn2.Text = "hoog naar laag";
             this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.Btn2_Click);
             // 
             // Form1
             // 
@@ -67,7 +70,7 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btn2);
             this.Controls.Add(this.btn1);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.lstBox);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -76,7 +79,7 @@
 
         #endregion
 
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox lstBox;
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btn2;
     }
--- CSharp/h1/2.1/Form1.cs
+++ CSharp/h1/2.1/Form1.cs
@@ -16,5 +16,37 @@ namespace _2._1
         {
             InitializeComponent();
         }
+        int[] getallenRij = new int[] { 2, 4, 6, 8, 10 };
+        //Listbox Afdrukken
+        private void afdrukkenRij(int[] getallenRij)
+        {
+            lstBox.Items.Clear();
+            for (int i = 0; i < getallenRij.Length; i++)
+            {
+                lstBox.Items.Add(getallenRij[i]);
+            }
+        }
+
+        //Laag naar hoog
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+
+            Array.Reverse(getallenRij);
+            afdrukkenRij(getallenRij);
+        }
+        //button Hoog naar laag
+        private void Btn2_Click(object sender, EventArgs e)
+        {
+            Array.Sort(getallenRij);
+            afdrukkenRij(getallenRij);
+
+        }
+       
+      
+
+        private void LstBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
