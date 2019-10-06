7275bc1914b71b48aa1ed02df291316626deaadf
pepijn <pepijn@pdik.nl>
2019-10-06 11:50:42 +0200

    Opdrachten

---
+++ testen/opdracht1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace opdracht1
+{
+    class Program
+    {
+        public static double verkoop(int inkoop)
+        {
+            double noBtw = inkoop + 300;
+            double totaal = noBtw * 1.21;
+            return totaal;
+        }
+        static void Main(string[] args)
+        {
+          
+            Console.WriteLine("Verkoop prijs");
+            double totaal = verkoop(1);
+            Console.Write(totaal);
+
+        }
+       
+}
+}
