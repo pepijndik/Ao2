5df95bd0589447c679b7e4699ec612cf8a648ab2
pepijn <pepijn@pdik.nl>
2019-10-11 13:08:35 +0200

    testen C#

--- testen/opdracht2/Program.cs
+++ testen/opdracht2/Program.cs
@@ -4,9 +4,21 @@ namespace opdracht2
 {
     class Program
     {
+        
+        public static int positief(int getal){
+            if(getal < 1){
+                     String text = "Getal is positief!";
+                  return text;
+            }
+            else if(getal < -1){
+                String text = "Getal is Negatief of nul";
+                  return text;
+}                     
+}
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            String text = positief(0);
+           Console.WriteLine(text);
         }
     }
 }
---
+++ testen/opdracht3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace opdracht3
+{
+    //Schrijf een functie genaamd Resultaat(int getal). Als een cijfer kleiner is dan 5.5 dan wordt er afgedrukt “onvoldoende”.
+    //Als het gelijk is aan 5.5 of hoger maar kleiner dan 8 dan wordt er afgedukt “ voldoende” . 
+    //En als het cijfer een 8 is of hoger dan wordt er afgedrukt “Goed”.
+    class Program
+    {
+        public void resultaat(int getal)
+        {
+            if(getal < 5.5)
+            {
+                Console.WriteLine("Onvoldoende");
+            }
+            else if(getal >= 5.5 && getal <8)
+            {
+                Console.WriteLine("Voldoende");
+            }else if (getal > 8)
+            {
+                Console.WriteLine("Goed");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+           resul
+        }
+    }
+}
