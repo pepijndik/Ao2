f8fcb8af75951936e180fab1ef5c79bd6ad58c46
pepijn <pepijn@pdik.nl>
2020-01-29 12:02:19 +0100

    Database opdrachten
    1.2 added and start of 1.3

--- CSharp/Database/1.2/Form1.Designer.cs
+++ CSharp/Database/1.2/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.txtaantal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(57, 45);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(664, 356);
+            this.listBox1.TabIndex = 0;
+            // 
+            // txtaantal
+            // 
+            this.txtaantal.Location = new System.Drawing.Point(68, 407);
+            this.txtaantal.Name = "txtaantal";
+            this.txtaantal.ReadOnly = true;
+            this.txtaantal.Size = new System.Drawing.Size(100, 22);
+            this.txtaantal.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtaantal);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox txtaantal;
     }
 }
 
--- CSharp/Database/1.2/Form1.cs
+++ CSharp/Database/1.2/Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.OleDb;
 namespace _1._2
 {
     public partial class Form1 : Form
@@ -15,6 +15,32 @@ namespace _1._2
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection();
+            con.ConnectionString = "provider = Microsoft.ACE.OLEDB.12.0;" + "Data Source = ..\\..\\Recepten.accdb;" +
+            "Persist Security Info = False;";
+            con.Open();
+
+            String query = "SELECT * FROM recept";
+
+            OleDbCommand comm = new OleDbCommand(query, con);
+
+            OleDbDataReader dbread = null;
+            dbread = comm.ExecuteReader();
+
+            var count = 0;
+            while (dbread.Read())
+            {
+                count++;
+                listBox1.Items.Add(dbread["Naam recept"].ToString());
+            }
+            txtaantal.Text = "Totaal aantal recepten: "+ count.ToString();
+
+            dbread.Close();
+            con.Close();
         }
     }
 }
