1388932f49e288e897e930b0f9515f89bafd16bd
pepijn <pepijn@pdik.nl>
2020-02-04 14:14:40 +0100

    java

---
+++ java/opdracht1/script.js
@@ -0,0 +1,26 @@
+// PTA cijfers informatica
+var ed01 = 5;
+var ed02 = "Dit heb jij goed gedaan!";
+var edmelding = 8.1;
+var gemiddelde = (ed01 + ed02) / 2;
+alert(gemiddelde);
+alert(edmelding);
+
+var getal = prompt("Geef een getal:");
+var getal2 = prompt("Geef een getal:");
+var eind = getal1 * getal2;
+//alert(eind);
+
+var optie1 = prompt("Number");
+var optie2 = prompt("Number2");
+
+var wissel1 = optie2;
+var wisse2 = optie1;
+optie1 = wisse2;
+optie2 = wisse2;
+alert(optie1 + " " + optie2);
+
+a.toFixed(1);
+parseInt(a);
+parseInt(a);
+toFixed();
