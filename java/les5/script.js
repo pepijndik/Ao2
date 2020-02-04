cbb85f2fb5df582a691dda6302da4497fc2ab2b6
pepijn <pepijn@pdik.nl>
2020-02-04 15:37:00 +0100

    les 5

--- java/les5/script.js
+++ java/les5/script.js
@@ -1,1 +1,6 @@
-function opdracht1() {}
+function opdracht1() {
+  var woord = "novacollege";
+  var lengte = woord.length / 2;
+  var x = Math.floor(lengte);
+  document.write(woord.substring(0, x));
+}
