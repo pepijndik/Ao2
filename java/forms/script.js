123b3cfa487e4cc07a747392b0983c668dd1d17f
pepijn <pepijn@pdik.nl>
2020-02-04 14:26:39 +0100

    Javascript

--- java/forms/script.js
+++ java/forms/script.js
@@ -3,3 +3,12 @@ function uitvoer() {
   var f = 1.8 * invoer + 32;
   alert(f);
 }
+function opdracht3() {
+  var invoer = document.forminvoer2.invoer.value;
+  var invoer2 = document.forminvoer2.invoer2.value;
+  var invoer3 = document.forminvoer2.invoer3.value;
+
+  var gem = invoer + invoer2 + invoer3 / 3;
+  gem.toFixed(1);
+  alert(gem);
+}
