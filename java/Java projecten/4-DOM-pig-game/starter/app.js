aeac0da88238d10e8b192d2e64bda64cdaf0b2ee
pepijn <pepijn@pdik.nl>
2020-02-05 17:02:54 +0100

    java project

--- java/Java projecten/4-DOM-pig-game/starter/app.js
+++ java/Java projecten/4-DOM-pig-game/starter/app.js
@@ -16,3 +16,11 @@ roundScore = 0;
 activePlayer = 1;
 
 dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector("#current-" + activePlayer).textContent = dice;
+
+var x = document.querySelector("#score-0").textContent;
+
+document.querySelector(".dice").style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener;
