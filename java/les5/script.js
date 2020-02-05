f0da340737ea4f20f6e9fbd5a96b13024f3ac60c
pepijn <pepijn@pdik.nl>
2020-02-05 15:02:45 +0100

    Javascript les 5

--- java/les5/script.js
+++ java/les5/script.js
@@ -16,27 +16,24 @@ function opdracht2() {
 }
 function opdracht3() {
   var string = "Novacollege is stom";
-  var vervangen = string.replace(/ [^ a, e, i, o, u, A, E, I, O, U] / g, '');
+  var vervangen = string.replace(/ [^ a, e, i, o, u, A, E, I, O, U] / g, "");
   alert(vervangen);
 }
 
-
-
 function opdracht4() {
   var getal = 12.0009;
- for(var i = 0; i < 2; i ++){
-   if(i = 1){
-     Math.ceil(getal);
-   }else {
-     Math.floor(getal);
-   }
- }
+  for (var i = 0; i < 2; i++) {
+    if ((i = 1)) {
+      Math.ceil(getal);
+    } else {
+      Math.floor(getal);
+    }
+  }
 }
 function opdracht5() {
-var numbers = new array();
+  var numbers = new array();
   for (var i = 0; i < 5; i++) {
     numbers.push(Math.random() * 20 + 5);
-
-   }
+  }
   alert(Math.max(numbers));
 }
