5dc22feb5a4e23478eb3d8a93d9a41aa777b01d8
pepijn <pepijn@pdik.nl>
2020-02-20 13:18:02 +0100

    C# login systeem

---
+++ CSharp/Database/loginsysteem/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace loginsysteem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txt_gbr = new System.Windows.Forms.TextBox();
+            this.txt_pass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txt_pass);
+            this.groupBox1.Controls.Add(this.txt_gbr);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(131, 58);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(402, 259);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Login";
+            this.groupBox1.UseWaitCursor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gebruikersnaam";
+            // 
+            // txt_gbr
+            // 
+            this.txt_gbr.Location = new System.Drawing.Point(151, 49);
+            this.txt_gbr.Name = "txt_gbr";
+            this.txt_gbr.Size = new System.Drawing.Size(100, 22);
+            this.txt_gbr.TabIndex = 1;
+            // 
+            // txt_pass
+            // 
+            this.txt_pass.Location = new System.Drawing.Point(151, 77);
+            this.txt_pass.Name = "txt_pass";
+            this.txt_pass.PasswordChar = '*';
+            this.txt_pass.Size = new System.Drawing.Size(100, 22);
+            this.txt_pass.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Wachtwoord";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_pass;
+        private System.Windows.Forms.TextBox txt_gbr;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CSharp/Database/loginsysteem/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+namespace loginsysteem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            OleDbConnection con1 = new OleDbConnection();
+            con1.ConnectionString = "provider = Microsoft.ACE.OLEDB.12.0;" + "Data Source = ..\\..\\loginsysteem.accdb;" +
+            "Persist Security Info = False;";
+            con1.Open();
+            OleDbDataReader dbread = null;
+            String query = "select * from gebruikers where gebruikersnaam= @gebruikersnaam AND wachtwoord = @wachtwoord";
+
+            OleDbCommand dbcom = new OleDbCommand(query, con1);
+            OleDbParameter param1 = new OleDbParameter();
+            param1.ParameterName = "@gebruikersnaam";
+            param1.Value = txt_gbr.Text;
+
+            OleDbParameter param2 = new OleDbParameter();
+            param2.ParameterName = "@wachtwoord";
+            param2.Value = txt_pass.Text;
+
+            dbcom.Parameters.Add(param1);
+            dbcom.Parameters.Add(param2);
+            dbread = dbcom.ExecuteReader();
+
+            if (dbread.HasRows)
+            {
+                MessageBox.Show("Ingelogd");
+            }
+            else
+            {
+                MessageBox.Show("Informatie is niet juist");
+            }
+
+        }
+
+    }
+}
