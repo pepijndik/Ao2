acea2d9b1344c9e28b3cf425429b641d9d489560
pepijn <pepijn@pdik.nl>
2020-02-20 22:09:15 +0100

    Update chat app

--- java/chat_app/server.js
+++ java/chat_app/server.js
@@ -1,1 +1,10 @@
 console.log("Hello world");
+
+// use express
+var express = require("express");
+
+// create instance of express
+var app = express();
+
+// use http with instance of express
+var http = require("http").createServer(app);
